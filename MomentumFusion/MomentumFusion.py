3f5fc397e8f747d6a3f2bd34bab1c41c60ce234d
Евгений Шляхтин <geniok@gmail.com>
2026-05-28 17:14:10 +0300

    Add MomentumFusion strategy

---
+++ MomentumFusion/MomentumFusion.py
@@ -0,0 +1,36 @@
+# MomentumFusion
+# Мульти-таймфрейм стратегия на основе моментума. Комбинирует сигналы с 1m, 5m, 15m для подтверждения тренда.
+# Type: Momentum | Timeframe: 1m, 5m, 15m | ML: False
+
+from freqtrade.strategy import IStrategy, DecimalParameter, IntParameter
+import pandas as pd
+import talib.abstract as ta
+
+class MomentumFusion(IStrategy):
+    timeframe = '1m'
+
+    minimal_roi = {
+        "0": 0.05,
+        "30": 0.03,
+        "60": 0.01,
+        "120": 0
+    }
+    stoploss = -0.02
+    trailing_stop = True
+    trailing_stop_positive = 0.01
+    trailing_stop_positive_offset = 0.02
+
+    buy_rsi = IntParameter(25, 40, default=30, space='buy')
+    sell_rsi = IntParameter(65, 80, default=70, space='sell')
+
+    def populate_indicators(self, dataframe: pd.DataFrame, metadata: dict) -> pd.DataFrame:
+        dataframe['rsi'] = ta.RSI(dataframe, timeperiod=14)
+        return dataframe
+
+    def populate_buy_trend(self, dataframe: pd.DataFrame, metadata: dict) -> pd.DataFrame:
+        dataframe.loc[(dataframe['rsi'] < self.buy_rsi.value), 'buy'] = 1
+        return dataframe
+
+    def populate_sell_trend(self, dataframe: pd.DataFrame, metadata: dict) -> pd.DataFrame:
+        dataframe.loc[(dataframe['rsi'] > self.sell_rsi.value), 'sell'] = 1
+        return dataframe
